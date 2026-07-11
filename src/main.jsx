12b01a009448c7dee67025390fed7ecb5e8bbf99
asm-saim <saim777m@gmail.com>
2026-07-11 08:06:54 +0600

    reacr routers root , children concept implemented

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './components/Root/Root.jsx'
+import Home from './components/Home/Home.jsx'
+import Mobiles from './components/Mobiles/Mobiles.jsx'
+import Laptop from './components/Laptop/Laptop.jsx'
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>"Hello from react router"</div>
+    path: '/',
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: "mobiles", Component: Mobiles },
+      { path: "laptop", Component: Laptop }
+    ]
   },
+  {
+    path: '/about',
+    element: <div>About section is here</div>
+  },
+  {
+    path: '/blogs',
+    Component: App
+  }
 ])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
