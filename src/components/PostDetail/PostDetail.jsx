80b08e0fccd869689505430722eb24bcdbae75ef
asm-saim <saim777m@gmail.com>
2026-07-11 22:28:40 +0600

    useParams hook implemented

--- src/components/PostDetail/PostDetail.jsx
+++ src/components/PostDetail/PostDetail.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate, useParams } from 'react-router';
 
 const PostDetail = () => {
-
     const detail = useLoaderData();
+
+    // if need , we can use hook  useParams
+    const { postid } = useParams()
+    console.log("params-Data", postid)
+
+    const navigate = useNavigate()
     return (
         <div>
             <h3>Post Detail</h3>
             <p>{detail.body}</p>
+            <button onClick={() => navigate(-1)}>Go Back</button>
         </div>
     );
 };
